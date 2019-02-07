723eaba8e154011d3f027d38728b505234994bfb
matthewkruzich <47336297+matthewkruzich@users.noreply.github.com>
2019-02-06 21:14:45 -0500

    Update company.sql

--- company.sql
+++ company.sql
@@ -2,14 +2,14 @@ create table company
   (name varchar(32) not null primary key,
    emails_sent int not null,
    sponsor_level varchar(32) not null,
-   foreign key(sponsor_level) references sponsor_status(metal) 
-   )
+   foreign key (sponsor_level) references sponsor_status (metal) 
+   );
    
 create table sponsor_status
   (metal varchar(32) not null primary key,
    amount int,
    email_cap int
-   )
+   );
    
  create table ad
   (job_title varchar(32) not null primary key,
@@ -17,24 +17,24 @@ create table sponsor_status
    province varchar(32),
    salary int,
    company varchar(32),
-   foreign key(company) references company(name) on delete cascade
-   )
+   foreign key (company) references company (name) on delete cascade
+   );
    
 /* Sample data */
 
---companies
+/* companies */
 insert into company values('Google', 4, 'Platinum');
 insert into company values('Facebook', 2, 'Gold');
 insert into company values('Microsoft', 3, 'Silver');
 insert into company values('Apple', 0, 'Bronze');
 
---status values
+/* status values */
 insert into status values('Platinum', 10000, 5); 
 insert into status values('Gold', 5000, 4); 
 insert into status values('Silver', 3000, 3); 
 insert into status values('Bronze', 1000, 0); 
 
---job ads
+/* job ads */
 insert into status values('Butcher', 'Kamloops', 'BC', 50000, 'Google'); 
 insert into status values('Baker', 'Barrie', 'ON', 60000, 'Facebook'); 
 insert into status values('Candlestick Maker', 'Saguenay', 'QC', 70000, 'Microsoft'); 
