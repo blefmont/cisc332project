fff1130d6b988da7513910760cfc05f05fcc19ba
Michael Olson <michael.olson@queensu.ca>
2019-03-25 10:14:03 -0400

    companies added

--- main_page.php
+++ main_page.php
@@ -6,8 +6,15 @@
 <body>
   <h1>Conference Database</h1>
   <p>
-  <a href="/jobs.php">Jobs</a> 
-  <a href="/companies.php">Companies</a> 
+  <a href="/jobs.php">Jobs</a>
+  <br><br>
+  <a href="/companies.php">Companies</a>
+  <br><br>
+  <a href="/sessions.html">Sessions</a>
+  <br><br>
+  <a href="/attendees.html">Attendees</a>
+  <br><br>
+  <a href="/attendees.html">Finances</a>
   </p>
 
 </body>
