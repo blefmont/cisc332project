10f99b97ba70940edbb8112efe75f5542be09860
jsc718 <47336288+jsc718@users.noreply.github.com>
2019-03-25 18:31:51 -0400

    alternative view for committee
    made the committee on the same page as the committee return

--- committee.php
+++ committee.php
@@ -4,12 +4,11 @@
   <link href="stylesheet.css" type="text/css" rel="stylesheet" >
 </head>
 <body>
-  <h1>Commitee Page</h1>
-  <p>Welcome</p>
-  <a href = "main_page.php">Main Page</a>
+  <h1>Committee Page</h1>
+  
 
   <p> Please Pick a Committee: </p>
-  <form action="committee_return.php" method="post">
+  <form action="committee.php" method="post">
   <?php
    #connect to database
    $pdo = new PDO('mysql:host=localhost;dbname=conference_database',"root","");
@@ -25,8 +24,43 @@
   ?>
   <input type="submit">
   </form>
+<br><br>
 
+
+<?php
+if (isset($_POST["committeeName"])){
+    $comm = $_POST["committeeName"];
+    echo "<p>Here is a list of the $comm members </p>";
+    echo "<p>The bolded entry is the committee leader </p>";
+    echo "<table><tr><th align=\"center\" colspan=\"2\">Name</th></tr>";
+
+    #connect to the database
+    $pdo = new PDO('mysql:host=localhost;dbname=conference_database', "root", "");
+
+    #first print the committee leader in bold 
+    $sql = "select chair_f, chair_l from committee where name = ?";
+    $stmt = $pdo -> prepare($sql); #create query 
+    $stmt-> execute([$comm]); #bind the parameters
+
+    #use fetch to get the committee leader 
+    $row = $stmt->fetch();
+    echo "<tr><td><b>".$row["chair_f"]."</b></td><td><b>".$row["chair_l"]."</b></td></tr>";
+
+    $sql = "select DISTINCT fname, lname from is_on WHERE committee_name = ? and (fname, lname) not in (SELECT chair_f as fname, chair_l as lname FROM committee where name=?)";
+    $stmt = $pdo->prepare($sql);   #create the query
+    $stmt->execute([$comm, $comm]);   #bind the parameters
+
+    #stmt contains the result of the program execution
+    #use fetch to get results row by row.
+    while ($row = $stmt->fetch()) {
+        echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td></tr>";
+    }
+}
+
+?>
+</table>
+<br><br>
+<a href = "main_page.php">Back to Main Page</a>
 </body>
 
 </html>
-
